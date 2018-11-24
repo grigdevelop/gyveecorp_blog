3b0e0f29da2fe28ecb0f6a4942df53533c51a986
grigdevelop <grigdevelop@gmail.com>
2018-11-24 15:35:34 +0100

    entity validation with tests

--- server/domain/validation/entity.validator.ts
+++ server/domain/validation/entity.validator.ts
@@ -1,15 +1,55 @@
 import Article from "../entities/article";
 import * as Joi from 'joi';
+import { ValidationResult } from "./validation.result";
+import { ValidationErrorItem } from "joi";
 
-let articleScheme = Joi.object().keys({
-    id: Joi.number().optional(),
+let articleSchema = Joi.object().keys({
+    id: Joi.number().error(() => '"id" most be a number.').optional(),
     title: Joi.string().min(3).max(100).required(),
     desc: Joi.string().required(),
     authorId: Joi.number().required(),
-    content: Joi.string().required(),
+    content: Joi.string().required().error(() => 'content is required'),
     datePublished: Joi.date().required()
 });
 
-let validateArticle = (article: Article) => {
-    return Joi.validate(article, articleScheme);
+let options: Joi.ValidationOptions = {
+    abortEarly: false
 };
+
+// validation method for all types of entity
+let validateEntity = <T>(entity: T, schema: Joi.ObjectSchema) : Promise<ValidationResult> => {
+    return new Promise<ValidationResult>(resolve => {
+
+        // use custom validation result
+        let validationResult: ValidationResult = new ValidationResult();
+
+        Joi.validate(entity, schema, options).then(result => {
+
+            // no validation error
+            resolve(validationResult);
+
+        }).catch(error => {   
+
+            // get strong types result of validation details
+            let details: ValidationErrorItem[] = error.details;
+
+            // set default message
+            validationResult.addMessage(error.message);
+
+            // add field validation errors to custom validation result
+            details.forEach(detail => {
+                validationResult.addError(detail.context.label, detail.message)
+            });
+
+            // resolve validation result
+            resolve(validationResult);
+        });
+
+    });
+}
+
+let validateArticle = (article: Article) : Promise<ValidationResult> => {
+    return validateEntity(article, articleSchema);
+};
+
+export { validateArticle }
