b6accb23d3b0c0a92ff2f7c1c0ed6f50f26861d8
grigdevelop <grigdevelop@gmail.com>
2018-11-24 13:56:55 +0100

    configuring solution

--- server/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // globalSetup: '<rootDir>/build/tests/setup/onAppStart.js',
-    // globalTeardown: '<rootDir>/build/tests/setup/onAppStop.js',
-    // setupFiles: ['<rootDir>/build/tests/setup/onTestStart.js']
-};
