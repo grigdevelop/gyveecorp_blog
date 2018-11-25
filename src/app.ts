1f35301ba293e3772e74f74cac41ef2486a6304b
grigdevelop <grigdevelop@gmail.com>
2018-11-25 19:37:42 +0100

    fixed debugger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import * as express from 'express';
 import {Express} from "express";
+import bodyParser = require('body-parser');
+import cors = require('cors');
 
 let app: Express = express();
+app.use(bodyParser.json());           
+app.use(cors());
+
 
 export default app;
